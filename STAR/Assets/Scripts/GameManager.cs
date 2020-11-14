f71f8e35d08f090a15ccfb402d9bf23f1492bdc9
JPCHATEAU01 <jp.chateau@yahoo.com>
2020-11-14 15:09:25 +0100

    Test
    add prefabs

---
+++ STAR/Assets/Scripts/GameManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public BoardManager boardScript;
+
+    private int level = 1;
+
+    void Awake()
+    {
+        boardScript = GetComponent<BoardManager>();
+        InitGame();
+    }
+
+    void InitGame()
+    {
+        boardScript.SetupScene(level);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
