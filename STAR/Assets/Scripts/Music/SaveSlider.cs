7cf0d37ec3a391f5de5ce170e9b34ae7ea64db4c
JPCHATEAU01 <72606011+JPCHATEAU01@users.noreply.github.com>
2020-12-07 16:40:27 +0100

    Add singletonMusic

---
+++ STAR/Assets/Scripts/Music/SaveSlider.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class SaveSlider
+    {
+        private static SaveSlider instance = null;// SINGLETON
+        //public Slider volumeSlider;
+
+        public SaveSlider()
+        {
+            
+        }
+
+        public static SaveSlider Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new SaveSlider();
+                }
+                return instance;
+            }
+        }
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
