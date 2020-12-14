e7b337e47adef47cc34ec8b4b19cb72a4ff31cc5
JPCHATEAU01 <72606011+JPCHATEAU01@users.noreply.github.com>
2020-12-14 18:33:54 +0100

    Credits

---
+++ STAR/Assets/Scripts/Music/MusicCredit.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class MusicCredit : MonoBehaviour
+    {
+        public GameObject musique;
+        AudioSource audioSource;
+        bool end;
+
+        void Awake()
+        {
+            Instantiate(musique);
+            audioSource = GameObject.Find("MusicCredit(Clone)").GetComponent<AudioSource>();
+            AudioListener.volume = SaveSlider.Instance.GetVolume();
+            end = false;
+
+        }
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (!audioSource.isPlaying && !end)
+            {
+                end = true;
+                GameManager.Instance().ReturnToMenu();
+                //Retour au Menu
+            }
+        }
+    }
+}
