233ff651623ee25f69ecfe8ef6a92d22b234c539
Nicolas <nicolas.kerneis29@gmail.com>
2020-12-14 10:42:11 +0100

    merge

--- STAR/Assets/Scripts/UI/boutiqueManager.cs
+++ STAR/Assets/Scripts/UI/boutiqueManager.cs
@@ -9,7 +9,7 @@ namespace Assets.Scripts
 {
     public class boutiqueManager : MonoBehaviour
     {
-
+        private int currentIndexItem;
         [SerializeField]
         private GameObject boutique;
 
@@ -25,6 +25,9 @@ namespace Assets.Scripts
         private string[] objectDescription;
         [SerializeField]
         private int[] objectPrice;
+        [SerializeField]
+        private GameObject[] textPrices;
+        public TMP_Text feedbackText;
 
         [SerializeField]
         private GameObject titrePopUp;
@@ -43,6 +46,10 @@ namespace Assets.Scripts
         {
             boutiqueLoadData();
             updateGoldDisplay();
+            for(int i=0; i<objectSprite.Length; i++)
+            {
+                
+            }
         }
 
         private void updateGoldDisplay()
@@ -57,30 +64,24 @@ namespace Assets.Scripts
             currentSelectedItemPrice = objectPrice[index];
             titrePopUp.GetComponent<TMP_Text>().text = "Achat de l'objet : " + objectName;
             textPopUp.GetComponent<TMP_Text>().text = "L'OBJECT COUTE " + currentSelectedItemPrice + " PIECES D'OR";
+            currentIndexItem = index;
         }
 
         private void boutiqueLoadData()
         {
-            int position = 0;
             for (int i = 0; i < items.Length; i++)
             {
                 Transform icon = items[i].transform.Find("Icon/objectIcon");
                 Image iconImage = icon.GetComponent<Image>();
-                iconImage.sprite = objectSprite[position];
+                iconImage.sprite = objectSprite[i];
                 Transform textName = items[i].transform.Find("Name/ObjectName");
                 TMP_Text textN = textName.GetComponent<TMP_Text>();
-                textN.text = objectName[position];
+                textN.text = objectName[i];
                 Transform textDescription = items[i].transform.Find("Description/Text (TMP)");
                 TMP_Text textD = textDescription.GetComponent<TMP_Text>();
-                textD.text = objectDescription[position];
-
-                //Position est un indice qui fait des boucle entre 0 et 3 simplement car le contenu n'est pas créer, donc il permet juste d'afficher qq info hardcodée
-
-                position++;
-                if (position == 3)
-                {
-                    position = 0;
-                }
+                textD.text = objectDescription[i];
+                TMP_Text textG = textPrices[i].GetComponent<TMP_Text>();
+                textG.text = objectPrice[i].ToString();
             }
         }
 
@@ -94,14 +95,48 @@ namespace Assets.Scripts
             if (checkIfAbleToBuy())
             {
                 MoneyForMerchant.Instance.RemoveMoney(currentSelectedItemPrice);
+                int augmentation = (int) (objectPrice[currentIndexItem] * 0.2);
+                objectPrice[currentIndexItem] = objectPrice[currentIndexItem] + augmentation;
+                switch (objectDescription[currentIndexItem])
+                {
+                    case "Augmentation des points de vie de la base":
+                        Base.Instance.AddMaxLp(500);
+                        break;
+                    case "Augmentation du revenu par seconde":
+                        Money.Instance.AddMoneyPerSecond();
+                        break;
+                    case "Augmentation de la limite d'argent":
+                        Money.Instance.AddMaxMoney(250);
+                        break;
+                    case "Diminution du temps de récupération du pouvoir":
+                        Spell.Instance.SetColldownTime();
+                        break;
+                    case "Augmentation du temps de freeze":
+                        Spell.Instance.SetSpellStunTime();
+                        break;
+                    case "Récupération de la vie":
+                        Base.Instance.ResetLp();
+                        break;
+                }
+                TMP_Text textG = textPrices[currentIndexItem].GetComponent<TMP_Text>();
+                textG.text = objectPrice[currentIndexItem].ToString();
+                feedbackText.gameObject.SetActive(true);
+                feedbackText.text = "Vous avez acheté l'objet " + objectName[currentIndexItem];
+                StartCoroutine(Wait());
+
+
             }
             else
             {
                 Debug.Log("pas assez d'argent");
             }
         }
+        IEnumerator Wait()
+        {
+            yield return new WaitForSeconds(4f);
 
-
+            feedbackText.gameObject.SetActive(false);
+        }
         public void OnClickreturnToShopButton()
         {
             boutique.SetActive(true);
